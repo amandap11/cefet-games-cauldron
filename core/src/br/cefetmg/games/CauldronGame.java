a189706759fe519fbf18f8400f34ac002147ee99
Amanda <afp1103@gmail.com>
2016-11-01 13:28:13 -0200

    pratica de particulas

--- core/src/br/cefetmg/games/CauldronGame.java
+++ core/src/br/cefetmg/games/CauldronGame.java
@@ -87,7 +87,8 @@ public class CauldronGame extends ApplicationAdapter {
 
         // solicita o carregamento dos efeitos de partículas
         ParticleEffectLoader.ParticleEffectLoadParameter loadParam = new ParticleEffectLoader.ParticleEffectLoadParameter(sistemaParticulas.getBatches());
-        assets.load("fogo.pfx", ParticleEffect.class, loadParam);
+        //assets.load("fogo.pfx", ParticleEffect.class, loadParam);
+        assets.load("fogoNovo.pfx", ParticleEffect.class, loadParam);
         assets.load("bolhas.pfx", ParticleEffect.class, loadParam);
         
         // solicita carregamento da música
@@ -104,7 +105,7 @@ public class CauldronGame extends ApplicationAdapter {
         sopa = caldeirao.getNode("topoDaSopa");
 
         // instancia, configura e dá início ao efeito de fogo
-        fogo = ((ParticleEffect) assets.get("fogo.pfx")).copy();
+        fogo = ((ParticleEffect) assets.get("fogoNovo.pfx")).copy();
         fogo.init();
         fogo.start();
         fogo.translate(new Vector3(0, 0.1f, 0));
@@ -112,9 +113,11 @@ public class CauldronGame extends ApplicationAdapter {
 
         // instancia, configura e dá início ao efeito das bolhas
         // use o campo ParticleEffect bolhas definido na linha #38
-        // ...
-        // ...
-        // ...
+        bolhas = ((ParticleEffect) assets.get("bolhas.pfx")).copy();
+        bolhas.init();
+        bolhas.start();
+        bolhas.translate(new Vector3(0, 0.6f, 0));
+        sistemaParticulas.add(bolhas);
         
         // começa a música
         musica.setLooping(true);
